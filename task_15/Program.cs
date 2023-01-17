84f10bd60483193b0f25a02d3251c9e07eea2c93
Ilya Plutov <ilya.plutov@gmail.com>
2023-01-17 23:01:29 +0300

    initial commit

---
+++ task_15/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+Console.Write("Введите номер дня недели ");
+int num = int.Parse(Console.ReadLine()!);
+if (num == 6 || num == 7) Console.WriteLine("ДА");
+else Console.WriteLine("НЕТ");
